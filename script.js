208225d540794418c1bf84890b17e3e34e412a8a
DEADRAVENOLEH <deadraven.reserve@gmail.com>
2025-03-31 10:00:54 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,29 @@
-// Прелоадер
+// Прелоадер с анимацией тату-машинки
 window.addEventListener("load", () => {
-    document.querySelector(".preloader").style.opacity = "0";
+    const preloader = document.querySelector(".preloader");
     setTimeout(() => {
-        document.querySelector(".preloader").style.display = "none";
-    }, 1000);
+        preloader.style.opacity = "0";
+        setTimeout(() => {
+            preloader.style.display = "none";
+        }, 1000);
+    }, 4000);
 });
+
+// Фоновый звук дождя
+const rainAudio = document.getElementById("rain-audio");
+rainAudio.volume = 0.3; // Уменьшаем громкость дождя
 
 // Анимация кнопок
 document.querySelectorAll('.button').forEach(button => {
     button.addEventListener('mouseover', () => {
         console.log('Навели на кнопку');
+    });
+});
+
+// Звук тату-машинки при наведении на кнопки
+document.querySelectorAll('.button').forEach(button => {
+    button.addEventListener('mouseenter', () => {
+        const sound = new Audio('assets/tattoo-machine.mp3');
+        sound.play();
     });
 });
