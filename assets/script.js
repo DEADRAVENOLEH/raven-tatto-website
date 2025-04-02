5253a143c87a7877d0f497b658aacb1249432a0f
DEADRAVENOLEH <deadraven.reserve@gmail.com>
2025-04-02 21:03:17 +0300

    Update script.js

--- assets/script.js
+++ assets/script.js
@@ -1,11 +1,5 @@
 document.addEventListener("DOMContentLoaded", function () {
-    const preloader = document.querySelector(".preloader");
-    const container = document.querySelector(".container");
-
-    setTimeout(() => {
-        preloader.classList.add("hidden"); // Убираем прелоадер
-        setTimeout(() => {
-            container.classList.add("loaded"); // Показываем контент
-        }, 500); // Задержка для плавного перехода
-    }, 2500);
+    setTimeout(function () {
+        document.getElementById("preloader").style.display = "none";
+    }, 2000);
 });
